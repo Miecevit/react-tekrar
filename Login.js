e974872eca8705933dc132e5a99ebf8dc9d1c689
Miecevit <mert.iecevit@gmail.com>
2023-03-27 15:11:59 +0300

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+
+function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const kullanici_adi = sessionStorage.getItem('email');
+        const sifre = sessionStorage.getItem('password');
+
+        if(email == kullanici_adi && password == sifre){
+
+            navigate('/portal');
+        }
+        else{
+            setError('Hatali kullanici adi veya sifre!');
+        }
+
+
+    }
+
+
+    return (
+        <div>
+            <h2>Giriş</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="email" 
+                        placeholder="Mail adresiniz" 
+                        value={email} 
+                        onChange= {(e) => setEmail(e.target.value)}
+                        required />
+                <input type="password"
+                        placeholder="Şifre"
+                        value={password}
+                        onChange= {(e) => setPassword(e.target.value)}
+                        required />
+                <button type="submit">Login</button>
+            </form>
+
+            {error && <p style={{color:'red'}}> {error} </p> }
+
+            <p>
+                Hesabınız yok mu? <Link to="/kayit">Kayıt Ol!</Link>
+            </p>
+        </div>
+    );
+}
+
+export default Login;
---
+++ Register_session.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function Register() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passRepeat, setPassRepeat] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(password != passRepeat){
+            setError("Sifreler eslesmiyor!");
+            return;
+        }
+
+        sessionStorage.setItem('email', email);
+        sessionStorage.setItem('password', password);
+
+        setEmail('');
+        setPassword('');
+        setPassRepeat('');
+        setSuccess('Kayit Olusturuldu. Giris yapabilirsiniz.');
+        setError('');
+    }
+
+    return (
+        <div>
+            <h2>Kayıt</h2>
+            <form onSubmit={handleSubmit}>
+            <input type="email" 
+                        placeholder="Mail adresiniz" 
+                        value={email} 
+                        onChange= {(e) => setEmail(e.target.value)}
+                        required />
+            <input type="password"
+                        placeholder="Şifre"
+                        value={password}
+                        onChange= {(e) => setPassword(e.target.value)}
+                        minLength = "6"
+                        required />
+            <input type="password"
+                        placeholder="Şifre Tekrar"
+                        value={passRepeat}
+                        onChange= {(e) => setPassRepeat(e.target.value)}
+                        minLength = "6"
+                        required />
+            <button type="submit">Kayıt Ol</button>
+            </form>
+            {error && <p style={{color: 'red'}}> {error} </p>}
+            {success && <p style={{color: 'green'}}> {success} </p>}
+            <p>
+                Hesabınız var mı? <Link to="/">Giriş Yap</Link>
+            </p>
+        </div>
+    );
+}
+
+export default Register;

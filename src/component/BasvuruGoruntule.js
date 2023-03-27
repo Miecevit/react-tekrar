51f6d0504ff583aed3a9df81d02f2b3503642417
Miecevit <mert.iecevit@gmail.com>
2023-03-27 14:59:51 +0300

    Delete src directory

--- src/component/BasvuruGoruntule.js
+++
@@ -1,9 +0,0 @@
-function BasvuruGoruntule(){
-
-    return(
-        <h1>BURADA BASVURU GORUNTULEME EKRANI VAR!</h1>
-    );
-
-}
-
-export default BasvuruGoruntule;
--- src/component/basvuruGoruntule.js
+++
@@ -1,9 +0,0 @@
-function basvuruGoruntule(){
-
-    return(
-        <h1>BURADA BASVURU GORUNTULEME EKRANI VAR!</h1>
-    );
-
-}
-
-export default basvuruGoruntule;

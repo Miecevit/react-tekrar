036f1290dcdee5fa3fb5de7c265018b9ca4b75a6
Miecevit <mert.iecevit@gmail.com>
2023-03-27 15:00:12 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter as Router, Route, Routes, Link, Outlet, Navigate} from 'react-router-dom';
+import Login from './Login.js';
+import Register from './Register.js';
+import Portal from './Portal.js';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/kayit" element={<Register />} />
+        <Route path="/portal" element={<Portal />} />
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ src/server/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "nodedb"
+});
+
+connection.connect((err) => {
+    if(err) {
+        console.error('Veritabanina baglanirken hata olustu: ', err);
+        return;
+    }
+    console.log('Veritabanina baglanildi.');
+});
+
+app.post('/kayit', (req,res) => {
+
+    const {email , password} = req.body;
+
+    const query = "INSERT INTO user (username,password) VALUE (?,?)";
+    
+    connection.query(query, [email, password], (err,result) => {
+
+        if(err){
+            console.error("Veritabanina bilgi girereken hata: ", err);
+            res.status(500).send({error: "Kayit olusturulurken bir hata olustu."});
+            return;
+        }
+        res.status(200).send({message: "Kayit basarili!"});
+
+    })
+    
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server ${PORT} üzerinde dinleniyor.`);
+});
+
+
+
